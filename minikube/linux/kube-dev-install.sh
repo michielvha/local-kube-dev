68814600e2023d8453732bcdff458ccb1edfe44e
michielvha <105451808+michielvha@users.noreply.github.com>
2025-05-06 22:22:13 +0200

    Update kube-dev-install.sh

--- minikube/linux/kube-dev-install.sh
+++ minikube/linux/kube-dev-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# THIS HAS BEEN MOVED TO PDS
 # minikube
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64/
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
